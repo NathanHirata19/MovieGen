ddda5196f339078eaebc2e68b0c4a1f656b02d5f
NaThAn H <stuff@whatever.com>
2016-10-07 13:49:42 -0700

    4

--- app/src/main/java/com/academy/app/moviegenerator/MainActivity.java
+++ app/src/main/java/com/academy/app/moviegenerator/MainActivity.java
@@ -1,24 +1,68 @@
 package com.academy.app.moviegenerator;
 
-import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.widget.TextView;
-
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView answerText;
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+    private float acceleration;
+    private float currentAcceleration;
+    private float previousAcceleration;
+
+    private final SensorEventListener sensorListener = new SensorEventListener(){
+    @Override
+        public void OnSensorChanged(SensorEvent event){
+
+
+    }
+        @Override
+        public void OnAccuracyChanged(Sensor sensor, int accuracy){
+
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sensorManager = (SensorManager)getSystemService(Context.SYSTEM_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        acceleration = 0.0f;
+        currentAcceleration = sensorManager.GRAVITY_EARTH;
+        previousAcceleration = sensorManager.GRAVITY_EARTH;
+
         answerText = (TextView) findViewById(R.id.answerText);
         answerText.setText(Generator.get().getGenerator());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(sensorListener, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
 }
+
+
+
+
+
+
+
+
